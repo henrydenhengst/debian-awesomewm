70b68cfcf027fc14e91583fb20f1c3622d9d1177
Henry den Hengst <henrydenhengst@gmail.com>
2022-09-07 13:46:24 +0200

    Update postinstall.sh

--- postinstall.sh
+++ postinstall.sh
@@ -13,10 +13,10 @@
 #
 #         USAGE:  bash postinstall.sh
 #
-#   DESCRIPTION:  Installation AwesomeWM on a clean vanilla Debian Sid (11)
+#   DESCRIPTION:  Installation AwesomeWM on a clean vanilla Cinnamon Debian Sid (11)
 #
 #       OPTIONS:  ---
-#  REQUIREMENTS:  a clean vanilla Debian Sid (11) installed, NO GUI
+#  REQUIREMENTS:  a clean vanilla Cinnamon Debian Sid (11) installed.
 #          BUGS:  ---
 #         NOTES:  ---
 #        AUTHOR:  Henry den Hengst , henrydenhengst@gmail.com
@@ -28,13 +28,12 @@
 #
 sudo nala fetch
 sudo nala update
-sudo nala install intel-microcode neovim mc git curl btop awesome lightdm kitty firefox-esr geany feh neofetch filezilla arandr conky suckless-tools mpc123 mpd -y
+sudo nala install intel-microcode firmware-linux neovim mc git curl btop awesome kitty geany feh neofetch filezilla arandr conky suckless-tools mpc123 mpd -y
 sudo nala install nitrogen firmware-iwlwifi picom polybar scrot unclutter xsel xbacklight volumeicon-alsa powerline fonts-powerline fonts-mononoki mpv vlc flameshot -y
 sudo nala install evince libreoffice strawberry inkscape gimp darktable lynx ghostwriter w3m syncthing handbrake pandoc pciutils pdf2svg pulseaudio alsa-utils -y
-sudo nala install flatpak kitty arp-scan asciinema bash-completion bat bettercap binutils btop coreutils crunch curl dbus dirb dmidecode exa ffmpeg file filezilla fish -y
-sudo nala install ghostscript git gnupg gparted gphoto2 hashcat hplip httrack iftop imagemagick jmtpfs keepassxc lshw lsof macchanger mc mtr nethogs nikto nmap p7zip -y
+sudo nala install flatpak kitty arp-scan asciinema bash-completion bat binutils btop coreutils crunch curl dbus dirb dmidecode exa ffmpeg file filezilla fish -y
+sudo nala install ghostscript git gnupg gparted gphoto2 hashcat hplip httrack iftop imagemagick jmtpfs keepassxc lshw lsof mc mtr nethogs nikto nmap p7zip -y
 sudo nala install proxychains qbittorrent ranger remmina ripgrep rsync smartmontools sqlmap tmux tomb traceroute tre-command tree unrar unzip usbutils vbetool vnstat -y
-sudo nala install rfkill bluez pulseaudio-module-bluetooth pulseaudio-module-gsettings pulseaudio-module-jack blueman -y
 sudo nala install yadm youtube-dl yt-dlp zip zsh fonts-crosextra-carlito fonts-crosextra-caladea wget whatweb whois xclip luarocks -y
 sudo nala install ttf-mscorefonts-installer libavcodec-extra gstreamer1.0-libav gstreamer1.0-plugins-ugly gstreamer1.0-vaapi ufw -y
 sudo nala install build-essential dkms linux-headers-$(uname -r) -y
@@ -50,7 +49,6 @@ flatpak install flathub com.github.Eloston.UngoogledChromium -y
 flatpak install flathub com.brave.Browser -y
 flatpak install flathub org.briarproject.Briar -y
 flatpak install flathub org.signal.Signal -y
-sudo sh <(curl -L https://nixos.org/nix/install) --daemon
 cd ~
 mkdir git
 cd git
@@ -58,4 +56,3 @@ git clone https://github.com/henrydenhengst/mynixos.git
 git clone https://github.com/henrydenhengst/debian-awesomewm.git
 cd ~
 sudo nala autoremove -y
-sudo nala autoclean -y
