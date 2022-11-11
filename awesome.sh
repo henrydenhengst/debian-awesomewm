ff86865bba856cbad7b342c34b07224f36da48f8
Henry den Hengst <henrydenhengst@gmail.com>
2022-11-11 13:28:24 +0100

    Update awesome.sh

--- awesome.sh
+++ awesome.sh
@@ -30,10 +30,12 @@
 #
 git clone https://github.com/henrydenhengst/wallpapers.git ~/wallpapers
 cp bashrc ~/.bashrc
+cp -r nvim/ ~/.config/nvim
+cp -r awesome/ ~/.config/awesome 
 sudo nala fetch
 sudo nala update
 sudo nala upgrade -y
-sudo nala install alsa-utils arandr arp-scan asciinema awesome bash-completion bat binutils btop cheese conky coreutils crunch curl dbus dirb dmidecode evince exa feh ffmpeg file firefox firmware-iwlwifi firmware-linux firmware-misc-nonfree fish flameshot flatpak fonts-crosextra-caladea fonts-crosextra-carlito fonts-mononoki fonts-powerline geany geany-plugins ghostscript git gnupg gparted gphoto2 gstreamer1.0-libav gstreamer1.0-plugins-ugly gstreamer1.0-vaapi hashcat hplip httrack iftop imagemagick intel-microcode jmtpfs keepassxc kitty libavcodec-extra lolcat lshw lsof luarocks lynx mc mpc123 mpd mpv mtr ncal neofetch neovim nethogs nikto nitrogen nmap nodejs npm p7zip pandoc pciutils pdf2svg picom polybar powerline printer-driver-all printer-driver-cups-pdf proxychains pulseaudio python3-pip python3-pynvim ranger remmina rename ripgrep rsync scrot sddm smartmontools sqlmap suckless-tools terminator tmux toilet tomb tor traceroute tre-command tree ttf-mscorefonts-installer ufw unclutter unrar unzip usbutils vbetool vlc vnstat volumeicon-alsa w3m wget whatweb whois xbacklight xclip xdotool xsel yadm youtube-dl yt-dlp zip zsh -y
+sudo nala install alsa-utils arandr arp-scan asciinema awesome bash-completion bat binutils btop cheese conky coreutils crunch curl dbus dirb dmidecode evince exa feh ffmpeg file firefox-esr firmware-iwlwifi firmware-linux firmware-misc-nonfree fish flameshot flatpak fonts-crosextra-caladea fonts-crosextra-carlito fonts-mononoki fonts-powerline geany geany-plugins ghostscript git gnupg gparted gphoto2 gstreamer1.0-libav gstreamer1.0-plugins-ugly gstreamer1.0-vaapi hashcat hplip httrack iftop imagemagick intel-microcode jmtpfs keepassxc kitty libavcodec-extra lolcat lshw lsof luarocks lynx mc mpc123 mpd mpv mtr ncal neofetch neovim nethogs nikto nitrogen nmap nodejs npm p7zip pandoc pciutils pdf2svg picom polybar powerline printer-driver-all printer-driver-cups-pdf proxychains pulseaudio python3-pip python3-pynvim ranger remmina rename ripgrep rsync scrot sddm smartmontools sqlmap suckless-tools terminator tmux toilet tomb tor traceroute tre-command tree ttf-mscorefonts-installer ufw unclutter unrar unzip usbutils vbetool vlc vnstat volumeicon-alsa w3m wget whatweb whois xbacklight xclip xdotool xsel yadm youtube-dl yt-dlp zip zsh -y
 sudo nala install build-essential dkms linux-headers-$(uname -r) -y
 sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 sudo systemctl enable sddm
@@ -48,8 +50,6 @@ flatpak install flathub io.lbry.lbry-app -y
 flatpak install flathub com.github.micahflee.torbrowser-launcher -y
 flatpak install flathub org.onionshare.OnionShare -y
 flatpak install flathub org.kde.kasts -y
-cp -r nvim/ ~/.config/nvim
-cp -r awesome/ ~/.config/awesome 
 sudo ufw allow ssh
 sudo ufw enable
 sudo reboot
