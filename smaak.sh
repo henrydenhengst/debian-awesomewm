c9c2a5d254e43a0b2ad6fc02fd189a24c5edc1aa
Henry den Hengst <henrydenhengst@gmail.com>
2023-01-20 10:14:21 +0100

    Update smaak.sh

--- smaak.sh
+++ smaak.sh
@@ -1,4 +1,4 @@
-sudo apt install nala
+sudo apt install nala -y
 
 sudo nala install clamav clamav-daemon clamtk unattended-upgrades arp-scan asciinema bash-completion bat binutils btop coreutils curl dmidecode evince exa feh ffmpeg file fish flameshot fonts-crosextra-caladea fonts-crosextra-carlito fonts-mononoki fonts-powerline geany ghostscript git gnupg gparted hashcat httrack iftop imagemagick keepassxc lolcat lshw lsof lynx mc mpc123 mtr ncal neofetch nethogs nmap p7zip pandoc pciutils pdf2svg powerline printer-driver-all printer-driver-cups-pdf proxychains ranger remmina rename ripgrep rsync smartmontools suckless-tools terminator tmux toilet tomb traceroute tre-command tree ttf-mscorefonts-installer ufw unrar unzip usbutils vlc w3m wget whois zip zsh -y
 
